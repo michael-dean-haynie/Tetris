42fd9dbbc1805f03ef7415c8ec0cc36cf03974d4
michael-dean-haynie <michael.dean.haynie@gmail.com>
2017-01-17 21:48:12 -0700

    Added starting points

--- functions.js
+++ functions.js
@@ -4,10 +4,35 @@ function addBlock(){
 	var newActiveBlockIndex = GS.blocks.length;
 
 	var newBlock = {
+		index: newActiveBlockIndex,
 		shape: shape,
-		index: newActiveBlockIndex
+		points: getStartingPoints(shape)
 	}
 
-	GS.blocks.append(newBlock);
+	GS.blocks.push(newBlock);
 	GS.activeBlockIndex = newActiveBlockIndex;
+}
+
+function getStartingPoints(shape){
+	switch (shape){
+		case "L":
+			return [{x:3,y:22},{x:3,y:21},{x:3,y:20},{x:4,y:20}];
+			break;
+
+		case "T":
+			return [{x:3,y:22},{x:3,y:21},{x:4,y:21},{x:3,y:20}];
+			break;
+
+		case "I":
+			return [{x:3,y:23},{x:3,y:22},{x:3,y:21},{x:3,y:20}];
+			break;
+
+		case "O":
+			return [{x:3,y:21},{x:4,y:21},{x:3,y:20},{x:4,y:20}];
+			break;
+
+		case "Z":
+			return [{x:3,y:21},{x:4,y:21},{x:4,y:20},{x:5,y:20}];
+			break;
+	}
 }
--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 function main(){
 	GS.mainCycleCount++;
-	console.log("Entering main() [Cycle="+GS.mainCycleCount+"]");
+	// console.log("Entering main() [Cycle="+GS.mainCycleCount+"]");
 
 	if(GS.gameIsOver === true){
 		// End game, show score, play again?
 	}
 
 	if(GS.activeBlockIndex === null){
-		// Create a new block
+		addBlock();
+		// console.log(GS.blocks);
 	}
 
 	// move block
 
 
-	console.log("Exiting main() [Cycle="+GS.mainCycleCount+"]");
+	// console.log("Exiting main() [Cycle="+GS.mainCycleCount+"]");
 }
