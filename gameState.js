20671fb81526746be3a447486822b308a674351c
michael-dean-haynie <michael.dean.haynie@gmail.com>
2017-01-25 18:17:39 -0700

    Starting fresh

--- gameState.js
+++
@@ -1,8 +0,0 @@
-var GS = {
-	mainLoopInterval: null,
-	mainCycleCount: 0,
-	gameIsOver: false,
-	activeBlockIndex: null,
-	blocks: [],
-	disableUserControl: false,
-}
--- main.js
+++ main.js
@@ -1,29 +1,3 @@
 function main(){
-	// GS.gameIsOver = true;
-	GS.mainCycleCount++;
-	// console.log("Entering main() [Cycle="+GS.mainCycleCount+"]");
 
-	if(!GS.gameIsOver){
-		if(GS.activeBlockIndex === null){
-			addBlock();
-		}
-
-		var doesWork = tryDropActiveBlock();
-		if (doesWork){
-			GS.blocks[GS.activeBlockIndex].handlePoint.y--;
-		} else {
-			GS.activeBlockIndex = null;
-		}
-
-		// checkForFullRows();
-
-		paintBoard();
-
-		GS.gameIsOver = checkIsGameOver();
-
-	} else {
-		clearInterval(GS.mainLoopInterval);
-		console.log("Game Over");
-		// display game over and points
-	}
 }
